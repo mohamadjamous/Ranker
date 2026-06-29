ac0af3daf335740a1d2372c64e37468d2da322b4
Mohamad Jamous <mohamad.e.jamous@gmail.com>
2026-06-29 13:10:29 +0300

    feat: creating a poll from react

--- client/src/state.ts
+++ client/src/state.ts
@@ -1,18 +1,23 @@
+import { Poll } from "shared/poll-types";
 import { proxy } from "valtio"
-
 
 
 export enum AppPage{
     Welcome = 'welcome',
     Create = 'create',
-    Join = 'join'
+    Join = 'join',
+    WaitingRoom = "waiting-room",
 };
 
 export type AppState = {
+    isLoading: boolean,
+    poll? : Poll,
+    accessToken?: string,
     currentPage : AppPage
 };
 
 const state : AppState = proxy({
+    isLoading: false,
     currentPage : AppPage.Welcome
 });
 
@@ -21,6 +26,21 @@ const actions = {
     setPage : (page : AppPage) : void =>{
         state.currentPage = page;
     },
+    startOver: () : void => {
+        actions.setPage(AppPage.Welcome);
+    },
+    startLoading: (): void => {
+        state.isLoading = true;
+    },
+    stopLoading: (): void => {
+        state.isLoading = false;
+    },
+    initializePoll: (poll? : Poll) : void =>{
+        state.poll = poll;
+    },
+    setPollAccessToken: (token: string) : void =>{
+        state.accessToken = token;
+    }
 };
 
 export {state, actions};
